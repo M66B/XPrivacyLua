f2cf5d74b81672dbb3f119301a94d5269198cf55
M66B <M66B@users.noreply.github.com>
2018-01-12 16:44:56 +0100

    Filter app info of other apps

---
+++ app/src/main/assets/generic_filter_by_uid.lua
@@ -0,0 +1,38 @@
+-- This file is part of XPrivacyLua.
+
+-- XPrivacyLua is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- XPrivacyLua is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with XPrivacyLua.  If not, see <http://www.gnu.org/licenses/>.
+
+-- Copyright 2017-2018 Marcel Bokhorst (M66B)
+
+function after(hook, param)
+    local list = param:getResult()
+    if list == nil then
+        return false
+    end
+
+    local index = 0
+    local filtered = false
+    while index < list:size() do
+        local item = list:get(index)
+        if hook:getName() == 'PackageManager.getInstalledPackages' then
+            item = item.applicationInfo
+        end
+        if item ~= nil and item.uid == param:getUid() then
+            index = index + 1
+        else
+            list:remove(index)
+        end
+    end
+    return filtered
+end
