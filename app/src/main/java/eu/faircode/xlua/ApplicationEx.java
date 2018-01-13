bf6683da96559057f103d1e4a1491eaf131e2be2
M66B <M66B@users.noreply.github.com>
2018-01-13 10:10:33 +0100

    Added application class

---
+++ app/src/main/java/eu/faircode/xlua/ApplicationEx.java
@@ -0,0 +1,33 @@
+/*
+    This file is part of XPrivacyLua.
+
+    XPrivacyLua is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XPrivacyLua is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XPrivacyLua.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2017-2018 Marcel Bokhorst (M66B)
+ */
+
+package eu.faircode.xlua;
+
+import android.app.Application;
+import android.util.Log;
+
+public class ApplicationEx extends Application {
+    private static final String TAG = "XLua.App";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "Create version=" + Util.getSelfVersionName(this));
+    }
+}
