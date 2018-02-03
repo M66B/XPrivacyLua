fa9b61360674c968df9a789a126693d32f9b5606
M66B <M66B@users.noreply.github.com>
2018-02-03 18:39:02 +0100

    Added hooks for network/SIM data

---
+++ app/src/main/assets/generic_operator_value.lua
@@ -0,0 +1,27 @@
+-- This file is part of XPrivacyLua.
+
+-- XPrivacyLua is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- XPrivacyLua is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with XPrivacyLua.  If not, see <http://www.gnu.org/licenses/>.
+
+-- Copyright 2017-2018 Marcel Bokhorst (M66B)
+
+function after(hook, param)
+    local result = param:getResult()
+    if result == nil then
+        return false
+    end
+
+    local fake = '00101' -- test network
+    param:setResult(fake)
+    return true, result, fake
+end
