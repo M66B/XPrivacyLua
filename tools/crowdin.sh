d0000635e6ac34419e1822b71117d8bd76be9d57
M66B <M66B@users.noreply.github.com>
2018-07-22 10:00:03 +0000

    Crowdin sync

--- tools/crowdin.sh
+++ tools/crowdin.sh
@@ -2,48 +2,28 @@
 . tools/config.sh
 
 #https://github.com/mendhak/Crowdin-Android-Importer
+#git clone https://github.com/mendhak/Crowdin-Android-Importer.git
+#sudo apt-get install python python-pip
+#sudo apt-get install libssl-dev libcurl4-openssl-dev
+#pip install pycurl
 
-rm -R ${project}/app/src/main/res/values-iw/
-rm -R ${project}/app/src/main/res/values-ar-rBH/
-rm -R ${project}/app/src/main/res/values-ar-rEG/
-rm -R ${project}/app/src/main/res/values-ar-rSA/
-rm -R ${project}/app/src/main/res/values-ar-rYE/
-rm -R ${project}/app/src/main/res/values-nb-rNO/
-rm -R ${project}/app/src/main/res/values-nn-rNO/
-rm -R ${project}/app/src/main/res/values-no-rNO/
+rm -R ${project_dir}/app/src/main/res/values-iw/
+rm -R ${project_dir}/app/src/main/res/values-ar-rBH/
+rm -R ${project_dir}/app/src/main/res/values-ar-rEG/
+rm -R ${project_dir}/app/src/main/res/values-ar-rSA/
+rm -R ${project_dir}/app/src/main/res/values-ar-rYE/
+rm -R ${project_dir}/app/src/main/res/values-fi*
 
-python $importer_dir/crowdin.py --p=app/src/main -a=get -i xprivacylua -k $api_key
+python ${importer_dir}/crowdin.py --p=app/src/main -a=get -i xprivacylua -k ${api_key}
 
-mkdir -p ${project}/app/src/main/res/values-iw/
-mkdir -p ${project}/app/src/main/res/values-ar-rBH/
-mkdir -p ${project}/app/src/main/res/values-ar-rEG/
-mkdir -p ${project}/app/src/main/res/values-ar-rSA/
-mkdir -p ${project}/app/src/main/res/values-ar-rYE/
+mkdir -p ${project_dir}/app/src/main/res/values-iw/
+mkdir -p ${project_dir}/app/src/main/res/values-ar-rBH/
+mkdir -p ${project_dir}/app/src/main/res/values-ar-rEG/
+mkdir -p ${project_dir}/app/src/main/res/values-ar-rSA/
+mkdir -p ${project_dir}/app/src/main/res/values-ar-rYE/
 
-cp -R ${project}/app/src/main/res/values-he/* \
-	${project}/app/src/main/res/values-iw/
-
-cp -R ${project}/app/src/main/res/values-ar/* \
-	${project}/app/src/main/res/values-ar-rBH/
-
-cp -R ${project}/app/src/main/res/values-ar/* \
-	${project}/app/src/main/res/values-ar-rEG/
-
-cp -R ${project}/app/src/main/res/values-ar/* \
-	${project}/app/src/main/res/values-ar-rSA/
-
-cp -R ${project}/app/src/main/res/values-ar/* \
-	${project}/app/src/main/res/values-ar-rYE/
-
-mkdir -p ${project}/app/src/main/res/values-nb-rNO/
-mkdir -p ${project}/app/src/main/res/values-nn-rNO/
-mkdir -p ${project}/app/src/main/res/values-no-rNO/
-
-cp -R ${project}/app/src/main/res/values-no/* \
-	${project}/app/src/main/res/values-nb-rNO/
-
-cp -R ${project}/app/src/main/res/values-no/* \
-	${project}/app/src/main/res/values-nn-rNO/
-
-cp -R ${project}/app/src/main/res/values-no/* \
-	${project}/app/src/main/res/values-no-rNO/
+cp -R ${project_dir}/app/src/main/res/values-he/* ${project_dir}/app/src/main/res/values-iw/
+cp -R ${project_dir}/app/src/main/res/values-ar/* ${project_dir}/app/src/main/res/values-ar-rBH/
+cp -R ${project_dir}/app/src/main/res/values-ar/* ${project_dir}/app/src/main/res/values-ar-rEG/
+cp -R ${project_dir}/app/src/main/res/values-ar/* ${project_dir}/app/src/main/res/values-ar-rSA/
+cp -R ${project_dir}/app/src/main/res/values-ar/* ${project_dir}/app/src/main/res/values-ar-rYE/
